6ec42debd1b395be4c93d8362cea0886c1373059
Jadsonvs <jadson.vsantos2013@gmail.com>
2022-07-14 14:32:52 -0300

    Introdução

---
+++ Capitulo01Introdução/src/Introducao.java
@@ -0,0 +1,56 @@
+
+public class Introducao {
+	
+/*
+ 
+-------------------------------------------------------------- PLATAFORMAS JAVA --------------------------------------------------------------  	
+
+                                                      JAVA SE, JAVA EE, JAVA ME, JAVA FX
+
+----- JAVA EE -----
+ 
+ CAMADA DE APRESENTAÇÃO:
+ - JavaServer Faces (JSF)
+  - JavaServer Pages (JSP)
+  
+ CAMADA DE CONTROLE: 
+ - Servlets
+ - Expression Language
+ - JavaServer Pages Standard Tag Lib
+ - Componentes JavaBeans
+ 
+ CAMADA DE NEGÓCIOS:
+ - Enterprise JavaBeans (EJB)
+ - JAX-RS RESTful web services
+ - JAX-WS web service endpoints
+ - Java Persistence API (JPA)
+ - Java EE managed beans
+ -
+ CAMADA DE DE PERSISTÊNCIA:
+ - Java Database Connectivity API (JDBC)
+ - Java EE Connector Architecture (JCA)
+ - Java Transaction API (JTA)
+ - Java Persistence API (JPA)
+ 
+ JAVA SE ->
+ 
+ - Java 3D
+ - Java Sound
+ - Java Advanced Imaging (JAI)
+ - AWT (Abstract Window Toolkit)
+ - JDesktop Integration Components (JDIC)
+ 
+ JAVAFX
+ 
+ - e(fx) eclipse
+ - FxForm2
+ - DataFX
+ - JFXtras
+ 
+ 
+ LINK PARA LEITURA: https://www.devmedia.com.br/introducao-as-plataformas-java/29544
+ 
+ 
+ */
+
+}
